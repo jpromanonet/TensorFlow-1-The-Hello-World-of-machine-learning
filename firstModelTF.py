22200aed11a10205ad558fffd60f66c355a01f4a
jpromanonet <programming@jpromano.net>
2019-12-16 12:28:27 -0300

    Finished

---
+++ firstModelTF.py
@@ -0,0 +1,22 @@
+# Importing Machine Learning and DS libraries
+
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+# Define and compile neural network
+
+model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+
+# Providing Data
+
+xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
+
+# Training the neural network
+
+model.fit(xs, ys, epochs=500)
+
+# Print the prediction
+print(model.predict([10.0]))
